f6e2c31d5d04fd61a9631b84cb25df60ca669df4
Leocario <nicolas.constantin@students.hevs.ch>
2020-05-19 15:35:41 +0200

    Ajoutez des fichiers projet.

---
+++ PrintPaymentSystemDN/UserControl1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace PrintPaymentSystemDN
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DN = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DN)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DN
+            // 
+            this.DN.Location = new System.Drawing.Point(281, 21);
+            this.DN.Name = "DN";
+            this.DN.Size = new System.Drawing.Size(100, 50);
+            this.DN.TabIndex = 0;
+            this.DN.TabStop = false;
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.DN);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(800, 450);
+            ((System.ComponentModel.ISupportInitialize)(this.DN)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox DN;
+    }
+}
