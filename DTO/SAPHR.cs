804eac849d9cb8743fdfa8db49587a504307510f
Leocario <nicolas.constantin@students.hevs.ch>
2020-05-19 16:17:47 +0200

    nu get + add DTO

---
+++ DTO/SAPHR.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class SAPHR
+    {
+        public int CardId { get; set; }
+
+        public int UserId { get; set; }
+
+        public String Username { get; set; }
+
+        public override string ToString()
+        {
+
+            return CardId + " : " + Username + " .-  (" + UserId + ")";
+
+        }
+    }
+}
